3c630ff48944bd2c660dcc9e78304bea5c678592
이윤슬 <yoonseul@interiorteacher.com>
2023-09-01 14:31:14 +0900

    홈페이지에 마크다운 데이터 페칭해오기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,48 +1,25 @@
 import React from "react";
 import Hero from "../components/home/Hero";
 import FeaturedPosts from "../components/home/FeaturedPosts";
+import { getFeaturedPosts } from "../lib/posts-util.js";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR. ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR. ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR. ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR. ",
-    date: "2022-02-10",
-  },
-];
-const HomePage = () => {
+const HomePage = ({ posts }) => {
+  console.log(posts);
   return (
     <main>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </main>
   );
 };
 
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
+}
 export default HomePage;
