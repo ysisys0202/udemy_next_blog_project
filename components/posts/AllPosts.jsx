dd4c2661e1fa2f058b69c1a15c4071a7b5fa8099
이윤슬 <yoonseul@interiorteacher.com>
2023-09-01 14:31:55 +0900

    모든 포스트 리스트 화면 추가

---
+++ components/posts/AllPosts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./AllPosts.module.css";
+import PostGrid from "../posts/PostGrid";
+const AllPosts = ({ posts }) => {
+  return (
+    <section className={classes.posts}>
+      <h1>All Posts</h1>
+      <PostGrid posts={posts} />
+    </section>
+  );
+};
+
+export default AllPosts;
