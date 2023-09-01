d0f73b7c404f5db04c264b9c2d29917df7af3c5e
이윤슬 <yoonseul@interiorteacher.com>
2023-09-01 14:45:12 +0900

    포스트 상세 페이지

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import PostContent from "../../components/posts/PostContent";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-  return <div></div>;
+const PostDetailPage = ({ post }) => {
+  return <PostContent post={post} />;
 };
 
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((postFileName) =>
+    postFileName.replace(/\.md$/, "")
+  );
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: true, // false or "blocking"
+  };
+}
+export function getStaticProps(context) {
+  const slug = context.params.slug;
+  console.log(context);
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+  };
+}
 export default PostDetailPage;
