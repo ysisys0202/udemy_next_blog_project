e532f21142f37f7a2c6a870a34289563fed83fc8
이윤슬 <yoonseul@interiorteacher.com>
2023-09-01 14:44:57 +0900

    PostContent 컴포넌트 만들기

---
+++ components/posts/PostContent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PostHeader from "./PostHeader";
+import classes from "./PostContent.module.css";
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customComponents = {
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+              className="image"
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
+    </article>
+  );
+};
+
+export default PostContent;
